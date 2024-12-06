0361884a14d545615e39bf85141ff82cc26ebe72
Tom Cassady <heytcass@gmail.com>
2024-12-06 15:29:51 -0500

    feat: add nixos-hardware with XPS 13 9370 support

--- flake.nix
+++ flake.nix
@@ -4,16 +4,22 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     
-    # Add Hyprland
-    hyprland.url = "github:hyprwm/Hyprland";
-    # This ensures Hyprland uses the same nixpkgs as we do
-    hyprland.inputs.nixpkgs.follows = "nixpkgs";
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # Add nixos-hardware
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  outputs = { self, nixpkgs, hyprland }: {
+  outputs = { self, nixpkgs, hyprland, nixos-hardware }: {
     nixosConfigurations."gti" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
+        # Add XPS 13 9370 hardware configuration
+        nixos-hardware.nixosModules.dell-xps-13-9370
+        
         hyprland.nixosModules.default
         ./hosts/gti
       ];
