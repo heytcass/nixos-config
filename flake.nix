b6d762b631bb6e70754169c9a21113af5ff8f89f
Tom Cassady <heytcass@gmail.com>
2024-12-06 14:39:58 -0500

    Add hyprland to flake.

--- flake.nix
+++ flake.nix
@@ -3,12 +3,18 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    
+    # Add Hyprland
+    hyprland.url = "github:hyprwm/Hyprland";
+    # This ensures Hyprland uses the same nixpkgs as we do
+    hyprland.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, hyprland }: {
     nixosConfigurations."gti" = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
+        hyprland.nixosModules.default
         ./hosts/gti
       ];
     };
