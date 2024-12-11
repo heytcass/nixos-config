3157225c06bdb3d682db242e024bd10070e8ca9a
Tom Cassady <heytcass@gmail.com>
2024-12-11 14:49:48 -0500

    refactor: remove redundant kernel parameters, rely on nixos-hardware

--- modules/boot/default.nix
+++ modules/boot/default.nix
@@ -34,12 +34,6 @@ with lib;
 
     # Add proper resume support
     boot.resumeDevice = "/dev/nvme0n1p3";
-    boot.kernelParams = [
-      "mem_sleep_default=deep"  # Force deep sleep mode
-      "nvme.noacpi=1"          # Help with NVME suspend issues
-      "acpi_rev_override=1"    # Fix potential ACPI issues
-      "acpi_sleep=deep"        # Additional parameter to enforce deep sleep
-    ];
     boot.initrd.supportedFilesystems = [ "encrypt" "ext4" ];
 
     # Add sbctl for debugging secure boot
