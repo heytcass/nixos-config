449c09fec8ed65135d1881661cab0ed7c425265c
Tom Cassady <heytcass@gmail.com>
2025-08-07 09:54:59 -0400

    Fix jasper module package references with overlay

--- modules/jasper.nix
+++ modules/jasper.nix
@@ -9,12 +9,17 @@
   # Enable jasper companion service
   services.jasperCompanion = {
     enable = true;
+    package = jasper.packages.${pkgs.system}.default;
     user = "tom";
     enableGnomeExtension = true;
   };
 
-  # Add the GNOME extension package to system packages
-  environment.systemPackages = [
-    jasper.packages.${pkgs.system}.gnome-extension
+  # The module will handle adding packages when enableGnomeExtension is true
+  # But we need to override the gnome extension package reference
+  nixpkgs.overlays = [
+    (final: prev: {
+      jasper-companion = jasper.packages.${pkgs.system}.default;
+      jasper-companion-gnome-extension = jasper.packages.${pkgs.system}.gnome-extension;
+    })
   ];
 }
